c900e97c7eb54821dc057e84db5412fc698d76ff
Clinston Antony <Clintusony@gmail.com>
2023-01-04 11:22:44 +0530

    form updates

--- src/about.jsx
+++ src/about.jsx
@@ -243,18 +243,20 @@ const About = () => {
 
             <h1 ><hr style={{ backgroundColor: '#06283D  ', height: 5 }} /></h1>
             <div className="contactus" align="center">
-                <h3 className='entryHeading'>Contact</h3>
-                {/* <i>e-mail: </i> */}
-                <input type='email' className='inputtag' placeholder='e-mail' required />
-                <br />
-                <input type='text' className='inputtag' placeholder='Your_Name' required />
-                <br />
-                {/* <i>Message: </i> */}
-                <textarea type='text' className='inputmsg' placeholder='Please enter your message here...' required rows={4} cols={30} />
-                <br />
-                <button mailto="">Send</button>
-                <br />
-                {/* //store message  in a file with date-time */}
+                <form name="contact" method="POST" data-netlify="true"> 
+                    <h3 className='entryHeading'>Contact</h3>
+                    {/* <i>e-mail: </i> */}
+                    <input type='email' className='inputtag' name="email" placeholder='e-mail' required />
+                    <br />
+                    <input type='text' className='inputtag' name="name" placeholder='Your_Name' required />
+                    <br />
+                    {/* <i>Message: </i> */}
+                    <textarea type='text' className='inputmsg' name="message" placeholder='Please enter your message here...' required rows={4} cols={30} />
+                    <br />
+                    <button type="submit">Send</button>
+                    <br />
+                    {/* //store message  in a file with date-time */}
+                </form>
             </div>
             <br />
         </Divone>
